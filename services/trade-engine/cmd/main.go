958b7585d92d10e07c0d35e455b21d9050e308db
Rehan <official.rehan.me@gmail.com>
2025-07-28 00:09:27 +0530

    main pushed

--- services/trade-engine/cmd/main.go
+++ services/trade-engine/cmd/main.go
@@ -2,26 +2,39 @@ package main
 
 import (
 	"context"
+	"fmt"
+
 	"trade-engine/internals/engine"
 	"trade-engine/internals/queue/redis"
 	"trade-engine/internals/utils"
 
 	"github.com/joho/godotenv"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 
-	godotenv.Load()
+	// load env variables
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Failed to load env")
+	}
 
+	// Initialize zerolog logger
 	utils.InitLogger()
+	log.Info().Msg("📄 Logger initialized")
 
+	// connect to redis
 	redis.ConnectRedis()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// Initialize engine
 	engine.InitEngine()
+	log.Info().Msg("Trade engine initialized")
 
 	redis.Consumer(ctx)
+
+	log.Info().Msg("🚀 Trade Engine started successfully")
 
 }
