1a6440a16ec5f3184b21d3ea97aa90016d0b320e
Rehan <official.rehan.me@gmail.com>
2025-08-07 21:00:04 +0530

    Add generate/url route for generating Presigned Url

--- services/api-service/src/app.ts
+++ services/api-service/src/app.ts
@@ -1,23 +1,21 @@
-import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
+import { Context, Hono } from 'hono';
 
 import { authRoutes } from '@/routes/auth';
 import { healthRoutes } from '@/routes/health';
+import { marketRoutes } from './routes/market';
 import { balanceRoutes } from './routes/balance';
 import { referralRoutes } from './routes/referral';
-import { verificationRoutes } from './routes/verification';
+import { categoriesRoutes } from './routes/categories';
+import { generatePresignedUrl } from './lib/aws/presign';
 import { transactionRoutes } from './routes/transaction';
+import { verificationRoutes } from './routes/verification';
 
 const app = new Hono();
 
 // middlewares
 app.use(logger());
-// app.use(
-// 	compress({
-// 		encoding: 'gzip',
-// 	}),
-// );
 app.use(
 	cors({
 		origin: ['http://localhost:5173'],
@@ -37,11 +35,41 @@ app.use('*', async (c, next) => {
 });
 
 // routes
-app.route('/api/v1/health', healthRoutes);
 app.route('/api/v1/auth', authRoutes);
+app.route('/api/v1/health', healthRoutes);
+app.route('/api/v1/market', marketRoutes);
 app.route('/api/v1/balance', balanceRoutes);
 app.route('/api/v1/referral', referralRoutes);
-app.route('/api/v1/verification', verificationRoutes);
+app.route('/api/v1/categories', categoriesRoutes);
 app.route('/api/v1/transaction', transactionRoutes);
+app.route('/api/v1/verification', verificationRoutes);
+
+/**
+ * this route is for generating Presigned Url, called by frontend in Create Event page
+ */
+
+app.post('/api/v1/generate/url', async (c: Context) => {
+	const body = await c.req.json();
+
+	const { fileName, fileType } = body;
+
+	if (!fileName || !fileType) {
+		return c.json(
+			{
+				success: false,
+				message: 'Missing file name or type',
+			},
+			400,
+		);
+	}
+
+	const { url, publicUrl } = await generatePresignedUrl(fileName, fileType);
+	return c.json({
+		success: true,
+		message: 'Presinges url generated',
+		url,
+		publicUrl,
+	});
+});
 
 export default app;
