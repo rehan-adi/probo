acec264c5c8bff1cb9339270a713d403a018b971
Rehan <official.rehan.me@gmail.com>
2025-08-12 19:29:00 +0530

    Improve zerolog logger config and update .env.example

--- services/trade-engine/internals/utils/logger.go
+++ services/trade-engine/internals/utils/logger.go
@@ -8,18 +8,48 @@ import (
 )
 
 func InitLogger() {
+
 	APP_ENV := os.Getenv("APP_ENV")
+	VERSION := os.Getenv("VERSION")
+	LOG_LEVEL := os.Getenv("LOG_LEVEL")
+	SERVICE_NAME := os.Getenv("SERVICE_NAME")
+
+	level, err := zerolog.ParseLevel(LOG_LEVEL)
+
+	if err != nil {
+		if APP_ENV == "dev" {
+			level = zerolog.DebugLevel
+		} else {
+			level = zerolog.InfoLevel
+		}
+	}
 
 	if APP_ENV == "dev" {
-		log.Logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout}).
+		log.Logger = zerolog.New(zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			NoColor:    false,
+			TimeFormat: "3:04:05PM",
+		}).
+			Level(level).
 			With().
+			Str("service", SERVICE_NAME).
+			Str("version", VERSION).
 			Timestamp().
+			Caller().
 			Logger()
 	} else {
 		zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 		log.Logger = zerolog.New(os.Stdout).
+			Level(level).
 			With().
+			Str("service", SERVICE_NAME).
+			Str("version", VERSION).
 			Timestamp().
 			Logger()
 	}
+	log.Logger = log.Logger.Hook(zerolog.HookFunc(func(e *zerolog.Event, level zerolog.Level, msg string) {
+		if level == zerolog.ErrorLevel || level == zerolog.FatalLevel {
+			e.Stack()
+		}
+	}))
 }
