a979f2516e7e3f6a14720ebac3adf79321ee7858
Rehan <official.rehan.me@gmail.com>
2025-08-16 19:55:19 +0530

    remove Strict mode

--- apps/web-app/src/main.tsx
+++ apps/web-app/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
@@ -6,9 +5,9 @@ import { queryClient } from '@/lib/queryClient.ts';
 import { QueryClientProvider } from '@tanstack/react-query';
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
+	<>
 		<QueryClientProvider client={queryClient}>
 			<App />
 		</QueryClientProvider>
-	</StrictMode>,
+	</>,
 );
