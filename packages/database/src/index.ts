f00131261e62f61e2f252a35fef791d5e61ca7e8
Rehan <official.rehan.me@gmail.com>
2026-07-14 18:34:09 +0530

    Remove query logging from prisma client initialization

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -7,7 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
 	globalForPrisma.prisma ??
 	new PrismaClient({
-		log: ['query', 'info', 'warn', 'error'],
+		log: ['info', 'warn', 'error'],
 	});
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
