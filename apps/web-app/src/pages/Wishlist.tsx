5701dbd0c1e91ed9addc79432ed48e3b1704fee3
rehan-adi <131847460+rehan-adi@users.noreply.github.com>
2026-08-16 16:05:20 +0000

    style: auto format code with Prettier [skip ci]

--- apps/web-app/src/pages/Wishlist.tsx
+++ apps/web-app/src/pages/Wishlist.tsx
@@ -31,8 +31,8 @@ export default function WishlistPage() {
 				.catch((err) => console.error('Error fetching watchlist', err))
 				.finally(() => setLoading(false));
 		} else {
-            setLoading(false);
-        }
+			setLoading(false);
+		}
 	}, [isAuthenticated]);
 
 	useEffect(() => {
@@ -79,7 +79,7 @@ export default function WishlistPage() {
 
 		try {
 			await api.delete(`/profile/watchlist/${marketId}`);
-			setEvents((prev) => prev.filter(event => event.id !== marketId));
+			setEvents((prev) => prev.filter((event) => event.id !== marketId));
 		} catch (error) {
 			console.error('Failed to remove bookmark', error);
 		}
@@ -94,68 +94,66 @@ export default function WishlistPage() {
 							<h1 className="text-xl font-semibold border-b border-gray-200 dark:border-gray-800 pb-3 mb-4 text-gray-900 dark:text-white">
 								My Wishlist
 							</h1>
-                            
-                            {loading ? (
-                                <p className="text-gray-500">Loading wishlist...</p>
-                            ) : events.length > 0 ? (
-                                <div
-                                    className={`flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4`}
-                                >
-                                    {events.map((event, idx) => (
-                                        <div
-                                            key={idx}
-                                            onClick={() => navigate(`/events/${event.symbol}`)}
-                                            className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 cursor-pointer rounded-xl p-4 flex flex-col justify-between gap-0 h-57.5 transition-colors"
-                                        >
-                                            <div className="">
-                                                <div className="flex items-center text-gray-600 dark:text-gray-400">
-                                                    <img src={barChartIcon} className="w-4 h-4 mr-1" />
-                                                    <p className="text-xs">{event.numberOfTraders || 0} traders</p>
-                                                </div>
 
-                                                <div className="flex gap-3 mt-4">
-                                                    <img
-                                                        src={event.thumbnail || defaultThumbnail}
-                                                        alt={event.title}
-                                                        className="w-14 h-14 object-cover rounded-lg border border-gray-100 dark:border-gray-800 shrink-0"
-                                                    />
-                                                    <h2 className="md:text-base text-sm font-medium line-clamp-3 leading-snug overflow-hidden text-gray-900 dark:text-white">
-                                                        {event.title}
-                                                    </h2>
-                                                </div>
-                                            </div>
+							{loading ? (
+								<p className="text-gray-500">Loading wishlist...</p>
+							) : events.length > 0 ? (
+								<div className={`flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4`}>
+									{events.map((event, idx) => (
+										<div
+											key={idx}
+											onClick={() => navigate(`/events/${event.symbol}`)}
+											className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 cursor-pointer rounded-xl p-4 flex flex-col justify-between gap-0 h-57.5 transition-colors"
+										>
+											<div className="">
+												<div className="flex items-center text-gray-600 dark:text-gray-400">
+													<img src={barChartIcon} className="w-4 h-4 mr-1" />
+													<p className="text-xs">{event.numberOfTraders || 0} traders</p>
+												</div>
 
-                                            <div className="flex flex-col gap-3">
-                                                <div className="flex gap-4 w-full">
-                                                    <button className="text-green-700 dark:text-green-400 cursor-pointer bg-green-50 dark:bg-green-900/30 text-xs px-3 py-3 rounded-md w-full font-bold transition hover:bg-green-100 dark:hover:bg-green-900/50">
-                                                        Yes ₹{event.yesPrice}
-                                                    </button>
-                                                    <button className="text-red-700 dark:text-red-400 cursor-pointer bg-red-50 dark:bg-red-900/30 text-xs px-3 py-3 rounded-md w-full font-bold transition hover:bg-red-100 dark:hover:bg-red-900/50">
-                                                        No ₹{event.noPrice}
-                                                    </button>
-                                                </div>
+												<div className="flex gap-3 mt-4">
+													<img
+														src={event.thumbnail || defaultThumbnail}
+														alt={event.title}
+														className="w-14 h-14 object-cover rounded-lg border border-gray-100 dark:border-gray-800 shrink-0"
+													/>
+													<h2 className="md:text-base text-sm font-medium line-clamp-3 leading-snug overflow-hidden text-gray-900 dark:text-white">
+														{event.title}
+													</h2>
+												</div>
+											</div>
 
-                                                <div className="flex items-center justify-between md:pt-0 pt-1">
-                                                    <p className="text-xs flex items-center justify-start gap-1.5 text-gray-500 dark:text-gray-400 font-medium">
-                                                        <span className="font-bold text-gray-900 dark:text-white">
-                                                            ₹{formatVolume(event.volume || 0)} Vol.
-                                                        </span>
-                                                    </p>
-                                                    <button
-                                                        onClick={(e) => removeBookmark(e, event.id)}
-                                                        className="p-1 text-black dark:text-white transition-colors"
-                                                    >
-                                                        <Bookmark
-                                                            size={18}
-                                                            fill="currentColor"
-                                                            className="hover:text-gray-600 dark:hover:text-gray-300 cursor-pointer"
-                                                        />
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
+											<div className="flex flex-col gap-3">
+												<div className="flex gap-4 w-full">
+													<button className="text-green-700 dark:text-green-400 cursor-pointer bg-green-50 dark:bg-green-900/30 text-xs px-3 py-3 rounded-md w-full font-bold transition hover:bg-green-100 dark:hover:bg-green-900/50">
+														Yes ₹{event.yesPrice}
+													</button>
+													<button className="text-red-700 dark:text-red-400 cursor-pointer bg-red-50 dark:bg-red-900/30 text-xs px-3 py-3 rounded-md w-full font-bold transition hover:bg-red-100 dark:hover:bg-red-900/50">
+														No ₹{event.noPrice}
+													</button>
+												</div>
+
+												<div className="flex items-center justify-between md:pt-0 pt-1">
+													<p className="text-xs flex items-center justify-start gap-1.5 text-gray-500 dark:text-gray-400 font-medium">
+														<span className="font-bold text-gray-900 dark:text-white">
+															₹{formatVolume(event.volume || 0)} Vol.
+														</span>
+													</p>
+													<button
+														onClick={(e) => removeBookmark(e, event.id)}
+														className="p-1 text-black dark:text-white transition-colors"
+													>
+														<Bookmark
+															size={18}
+															fill="currentColor"
+															className="hover:text-gray-600 dark:hover:text-gray-300 cursor-pointer"
+														/>
+													</button>
+												</div>
+											</div>
+										</div>
+									))}
+								</div>
 							) : (
 								<p className="text-gray-500">Your wishlist is empty.</p>
 							)}
